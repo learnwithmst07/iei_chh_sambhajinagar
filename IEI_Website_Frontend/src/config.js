6dc58be10c7cbb4527c0b0a554db62a94697d8f3
learnwithmst07 <learnwithmst07@gmail.com>
2026-05-06 11:55:32 +0530

    updated backend url

--- IEI_Website_Frontend/src/config.js
+++ IEI_Website_Frontend/src/config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = 'https://iei-production.up.railway.app';
+// const API_BASE_URL = 'https://iei-production.up.railway.app';
+const API_BASE_URL = 'https://iei-chh-sambhajinagar.onrender.com';
 
 export default API_BASE_URL;
